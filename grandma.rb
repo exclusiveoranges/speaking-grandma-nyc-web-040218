ae158c4f1da1e0eee714280493dc6e87a604cc49
Ryan O'Toole <otoolerj@gmail.com>
2018-03-20 21:04:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    HUH?! SPEAK UP, SONNY!
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-    NO, NOT SINCE 1938!
+    "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
-    I LOVE YOU TOO PUMPKIN!
+    "I LOVE YOU TOO PUMPKIN!"
   end
 
 end
